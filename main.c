5aa181f9da93b04e35fd49a6d27090d297bc567d
Anil <mawjianil1@gmail.com>
2021-05-09 19:59:16 -0600

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main() {
     Array a = arrayInit(3);
-    arrayInsertAll(&a, 3, (int[]){2, 6, 1});
+    arrayInsertAll(&a, 6, (int[]){2, 6, 1, 4, 5, 3});
 
     arrayPrint(&a);
     insertionSort(a.array, a.length);
