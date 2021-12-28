b7ab7b0233adcdbf4bdde393bcf08b1d6787e38c
Anil <mawjianil1@gmail.com>
2021-12-27 17:33:20 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dynamic_array.h"
-#include "sorting_algs.h"
 
 int main() {
     Array a = arrayCreate(3);
     arrayInsertAll(&a, 6, (int[]){2, 6, 1, 4, 5, 3});
-
-    arrayPrint(&a);
-    insertionSort(a.array, a.length);
     arrayPrint(&a);
 
     return 0;
